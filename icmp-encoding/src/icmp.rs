a4c81812f307442d7198f7f9dc3c368b1d5870e9
Martin Charles <martincharles07@gmail.com>
2020-04-26 13:23:56 -0700

    Added Tests for ICMP Encoding and Decoding

--- icmp-encoding/src/icmp.rs
+++ icmp-encoding/src/icmp.rs
@@ -1,4 +1,9 @@
+use internet_checksum::{
+    checksum,
+    Checksum,
+};
 use nom::{
+    combinator::verify,
     error::ErrorKind,
     number::complete::{
         be_u16,
@@ -7,14 +12,12 @@ use nom::{
     },
     Err,
 };
-use internet_checksum::Checksum;
-use nom::combinator::verify;
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct IcmpV4Header {
-    message_type: u8,
-    code: u8,
-    remaining: u32,
+    pub message_type: u8,
+    pub code: u8,
+    pub remaining: u32,
 }
 
 impl IcmpV4Header {
@@ -30,7 +33,9 @@ impl IcmpV4Header {
 
         let (state, message_type) = be_u8(state)?;
         let (state, code) = be_u8(state)?;
-        let (state, _) = verify(be_u16, |actual_checksum| *actual_checksum == expected_checksum)(state)?;
+        let (state, _) = verify(be_u16, |actual_checksum| {
+            *actual_checksum == expected_checksum
+        })(state)?;
         let (state, remaining) = be_u32(state)?;
 
         Ok((
@@ -41,5 +46,98 @@ impl IcmpV4Header {
             },
             state,
         ))
+    }
+
+    pub fn encode(&self, buffer: &mut WritableIcmpPacket) {
+        // Write Header
+        {
+            let header = buffer.header();
+            header[0] = self.message_type;
+            header[1] = self.code;
+
+            header[4..8].clone_from_slice(&self.remaining.to_be_bytes());
+        }
+
+        // Checksum
+        let calculated_checksum = checksum(buffer.full());
+
+        // Insert Checksum
+        {
+            let header = buffer.header();
+            header[2..4].clone_from_slice(&calculated_checksum);
+        }
+    }
+}
+
+pub struct WritableIcmpPacket<'a> {
+    buffer: &'a mut [u8],
+}
+
+impl WritableIcmpPacket<'_> {
+    pub fn new(buffer: &mut [u8]) -> Option<WritableIcmpPacket> {
+        if buffer.len() < 8 {
+            None
+        } else {
+            Some(WritableIcmpPacket { buffer })
+        }
+    }
+
+    pub fn header(&mut self) -> &mut [u8] {
+        &mut self.buffer[0..8]
+    }
+
+    pub fn data(&mut self) -> &mut [u8] {
+        &mut self.buffer[8..]
+    }
+
+    pub fn full(&mut self) -> &mut [u8] {
+        &mut self.buffer
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        icmp::WritableIcmpPacket,
+        IcmpV4Header,
+    };
+
+    fn encoded() -> [u8; 64] {
+        [
+            0, 0, 131, 227, 32, 5, 0, 15, 94, 165, 50, 160, 0, 0, 223, 191, 8, 9, 10, 11, 12, 13,
+            14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35,
+            36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+        ]
+    }
+
+    fn decoded() -> IcmpV4Header {
+        IcmpV4Header {
+            message_type: 0,
+            code: 0,
+            remaining: 537198607,
+        }
+    }
+
+    #[test]
+    fn decode() {
+        let encoded = &encoded()[..];
+        let (header, body) = IcmpV4Header::decode(encoded).unwrap();
+
+        assert_eq!((header, body), (decoded(), &encoded[8..]))
+    }
+
+    #[test]
+    fn encode() {
+        let decoded = decoded();
+
+        let mut output_buffer = [0u8; 64];
+        let encoded = &encoded()[..];
+
+        let mut wrapped_output_buffer = WritableIcmpPacket::new(&mut output_buffer).unwrap();
+        wrapped_output_buffer.data().clone_from_slice(&encoded[8..]);
+
+        decoded.encode(&mut wrapped_output_buffer);
+
+        assert_eq!(encoded, wrapped_output_buffer.full(),)
     }
 }
--- icmp-encoding/src/ipv4.rs
+++ icmp-encoding/src/ipv4.rs
@@ -13,9 +13,9 @@ use nom::{
     IResult,
 };
 
-#[derive(Debug)]
+#[derive(PartialEq, Debug)]
 pub struct IpV4PacketHeader {
-    protocol: u16,
+    pub protocol: u16,
 }
 
 const IP_VERSION: u8 = 4;
@@ -26,7 +26,7 @@ impl IpV4PacketHeader {
             let (state, _) = tag(IP_VERSION, 4usize)(state)?;
             let (state, internet_header_length) = take::<_, u8, _, _>(4usize)(state)?;
             let (state, _) = take::<_, u8, _, _>(8usize)(state)?;
-            let (state, total_length) = bytes::<_, _, (&[u8], ErrorKind), _, _>(be_u16)(state)?;
+            let (state, _total_length) = bytes::<_, _, (&[u8], ErrorKind), _, _>(be_u16)(state)?;
 
             let (state, _) = take::<_, u32, _, _>(32usize)(state)?;
 
@@ -45,5 +45,27 @@ impl IpV4PacketHeader {
         })(packet)?;
 
         Ok((header, remaining))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::IpV4PacketHeader;
+
+    fn encoded() -> [u8; 84] {
+        [
+            69, 0, 64, 0, 101, 92, 0, 0, 64, 1, 253, 194, 10, 100, 1, 1, 10, 100, 1, 194, 0, 0,
+            131, 227, 32, 5, 0, 15, 94, 165, 50, 160, 0, 0, 223, 191, 8, 9, 10, 11, 12, 13, 14, 15,
+            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
+            38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55,
+        ]
+    }
+
+    #[test]
+    fn decode() {
+        let encoded = &encoded()[..];
+        let decoded = IpV4PacketHeader::decode(encoded).unwrap();
+
+        assert_eq!((IpV4PacketHeader { protocol: 1 }, &encoded[20..]), decoded)
     }
 }
